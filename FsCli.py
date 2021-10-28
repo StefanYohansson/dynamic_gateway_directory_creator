e3cb88a707e6eb190dd1392265ee517fe96bfd2f
StefanYohansson <sy.fen0@gmail.com>
2021-10-29 00:45:00 +0100

    add FsCli to run commands using fs cli

---
+++ FsCli.py
@@ -0,0 +1,23 @@
+import subprocess
+
+class FsCli():
+    cli_path = None
+
+    def __init__(self, cli_path):
+        self.cli_path = cli_path
+
+    def __get_cli_location(self):
+       return self.cli_path 
+
+    def __execute(self, cmd):
+        fs_cli = self.__get_cli_location()
+        subprocess.run([fs_cli, "-x"] + cmd)
+
+    def reloadxml(self):
+       self.__execute(["reloadxml"]) 
+
+    def rescan(self):
+        self.__execute(["sofia profile internal rescan reloadxml"])
+
+cli = FsCli("/home/snotr/Projects/telnyx_b2bua_builder/out-docker-vm/freeswitch/bin/fs_cli")
+cli.rescan()
